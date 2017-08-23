9dc08275723750bb0c78898c691b96141e3fdca7
FRaccoon <akihiko.arai@amail.plala.or.jp>
2017-08-23 17:27:55 +0900

    Promiseを使うように変更.

--- src.js
+++ src.js
@@ -1,33 +1,31 @@
 
-window.onload = function() {
-  var cs = document.getElementById('map');
-  var ctx = cs.getContext('2d');
+window.onload = async function() {
+  var cs = document.getElementById('map')
+  var ctx = cs.getContext('2d')
   
-  draw_map(cs, ctx, './map.png', [[.17, .25], [.20, .25], [.20, .50], [.46, .50], [.46, .56]]);
+  await draw_map(cs, ctx, './map.png')
+  draw_route(cs, ctx, [[.17, .25], [.20, .25], [.20, .50], [.46, .50], [.46, .56]])
   
-};
+}
 
-var draw_map = function(cs, ctx, img_src, route) {
-  var map_img = new Image();
-  map_img.src = img_src;
-  
-  map_img.onload = function() {
-    ctx.drawImage(map_img, 0, 0, cs.width, cs.height);
-    draw_route(cs, ctx, route);
-  };
-  
-};
+function draw_map(cs, ctx, img_src) {
+  return new Promise((resolve) => {
+    var map_img = new Image()
+    map_img.src = img_src
+    map_img.onload = () => {
+      ctx.drawImage(map_img, 0, 0, cs.width, cs.height)
+      resolve()
+    }
+  })
+}
 
-var draw_route = function(cs, ctx, route) {
-  ctx.strokeStyle = 'rgb(255, 0, 0)';
-  
-  ctx.beginPath();
-  
-  ctx.moveTo(route[0][0]*cs.width, route[0][1]*cs.height);
+function draw_route(cs, ctx, route) {
+  ctx.strokeStyle = 'rgb(255, 0, 0)'
+  ctx.beginPath()
+  ctx.moveTo(route[0][0]*cs.width, route[0][1]*cs.height)
   for(var i=0;i<route.length;i++) {
-    ctx.lineTo(route[i][0]*cs.width, route[i][1]*cs.height);
+    ctx.lineTo(route[i][0]*cs.width, route[i][1]*cs.height)
   }
-  
-  ctx.stroke();
-};
+  ctx.stroke()
+}
 
